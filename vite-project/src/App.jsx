519c9afa24cda68e7859d2c580ee36a3e18ee988
bharathi-003 <bharathiananth@gmail.com>
2026-02-07 16:02:54 +0530

    upload

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,15 @@
 import Course from './Course'
 import './App.css'
+import HTML from './assets/HTML.jpg'
+import CSS from './assets/CSS.jpg'
+import JAVASCRIPT from './assets/JAVASCRIPT.jpg'
 
 function App() {
-  
-
   return (
     <>
-      <Course />
+      <Course name="HTML" price="$100" image={HTML} rating={8} show={true}/>
+       <Course name="CSS" price= "$200" image={CSS} rating={8.5}/>
+        <Course name="JAVASCRIPT" price="$500" rating={9.0}image={JAVASCRIPT} show={true}/>
     </>
   )
 }
--- vite-project/src/Course.jsx
+++ vite-project/src/Course.jsx
@@ -1,13 +1,24 @@
-import SURIYA1 from './assets/SURIYA1.jpg'
+
 import './Course.css'
-const Course1="HTML"
-function Course(){
-    return(
-        <div className="div">
-            <img src={SURIYA1} alt=""></img>
-            <h1>{Course1}</h1>
-            <p>This code is Code io related program</p>
-        </div>
-    );
+const Course1 = "HTML"
+function Course(props) {
+    if (props.show == true){
+        return (
+            <div className="div">
+                <img src={props.image} alt=""></img>
+                <h1>{props.name}</h1>
+                <p>{props.price}</p>
+                <p>{props.rating}</p>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div className='div'>Course not available</div>
+        )
+    }
+
 }
+
+
 export default Course;
